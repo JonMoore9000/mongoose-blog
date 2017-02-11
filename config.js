b7266893b1c3136ec215f1277f6dcc156f592c00
JonMoore9000 <jonmoore9000@gmail.com>
2017-02-11 10:24:39 -0600

    adding tests

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
 					  'mongodb://Jon:allison9@ds145649.mlab.com:45649/blog-challenge';
+
+exports.TEST_DATABASE_URL = (
+	process.env.TEST_DATABASE_URL ||
+	'mongodb://Jon:allison9@ds145649.mlab.com:45649/blog-challenge');
+
 exports.PORT = process.env.PORT || 8080;
